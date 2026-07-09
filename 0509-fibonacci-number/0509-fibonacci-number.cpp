10a95670967ea4c6c76aa22dcb40212c3ea45036
sonalipaul <sonalipaul461@gmail.com>
2026-07-09 11:06:45 +0530

    Time: 4 ms (53.67%), Space: 8.2 MB (16.31%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    int fibonacci(int n,vector<int>&dp){
-        if(n<=1)return n;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]= fibonacci(n-1,dp)+fibonacci(n-2,dp);
+    // int fibonacci(int n,vector<int>&dp){
+    //     if(n<=1)return n;
+    //     if(dp[n]!=-1)return dp[n];
+    //     return dp[n]= fibonacci(n-1,dp)+fibonacci(n-2,dp);
 
-    }
+    // }
     int fib(int n) {
+        if(n<=1)return n;
         vector<int>dp(n+1,-1);
-         return fibonacci(n,dp);
+        //base case fill in bottom up approch 
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+         return dp[n];
     }
 };
