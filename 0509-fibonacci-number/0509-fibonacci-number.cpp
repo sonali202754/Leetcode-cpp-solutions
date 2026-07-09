025d4ccf30398060a153421330c21b983a13d627
sonalipaul <sonalipaul461@gmail.com>
2026-07-09 10:58:19 +0530

    Time: 14 ms (23.03%), Space: 7.7 MB (64.81%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int fibonacci(int n){
+        if(n<=1)return n;
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+    int fib(int n) {
+         return fibonacci(n);
+    }
+};
