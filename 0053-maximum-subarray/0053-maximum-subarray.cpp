a4eb503fa5baf81aed1d5592820a73c3070e51d5
sonalipaul <sonalipaul461@gmail.com>
2026-07-10 15:38:47 +0530

    Time: 0 ms (100%), Space: 71.7 MB (52.27%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+      //better approch
+    //   int n= nums.size();
+    //   int maxi= INT_MIN;
+    //   for(int i=0;i<n;i++){
+    //     int sum=0;
+    //     for(int j=i;j<n;j++){
+    //         sum+=nums[j];
+    //          maxi= max(maxi,sum);
+    //     }
+       
+    //   }  
+    //   return maxi;
+    //optimal solution using kardans Algorithm
+    int n= nums.size();
+    int sum=0,maxi=INT_MIN;
+    for(int i=0; i<n; i++){
+        sum+=nums[i];
+    
+    if(sum>maxi){
+        maxi=sum;
+    }
+    if(sum<0){
+        sum=0;
+    }
+    }
+    return maxi;
+    }
+};
