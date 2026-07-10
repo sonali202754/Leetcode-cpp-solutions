86a516e8f44a31ce5c2d3a23915b68a8ae402b1b
sonalipaul <sonalipaul461@gmail.com>
2026-07-10 15:16:18 +0530

    Time: 0 ms (100%), Space: 28.3 MB (10.23%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,14 +4,29 @@ public:
     //  sort(nums.begin(), nums.end());
     //  return nums [ (nums.size()/2)];  
      // brute 
-     int n= nums.size();
-     for(int i=0;i<n;i++){
-       int count=1;
-       for(int j=i+1; j<n;j++){
-        if(nums[i]==nums[j])count++;
-       }
-       if(count>n/2) return nums[i];
-     }
+    //  int n= nums.size();
+    //  for(int i=0;i<n;i++){
+    //    int count=1;
+    //    for(int j=i+1; j<n;j++){
+    //     if(nums[i]==nums[j])count++;
+    //    }
+    //    if(count>n/2) return nums[i];
+    //  }
+    //  return -1;
+    // }
+    // // better approch
+    int n= nums.size();
+    map<int,int>mp;
+    for(int i=0; i<n;i++){
+        mp[nums[i]]++;
+    }
+    for(auto it :mp){
+        if(it.second>n/2){
+            return it.first;
+        }
+
+    }
      return -1;
     }
+
 };
