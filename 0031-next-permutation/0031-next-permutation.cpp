acf88ffccb6e942a8b4cad22efcde88897b0478d
sonalipaul <sonalipaul461@gmail.com>
2026-08-16 13:28:44 +0530

    Time: 0 ms (100%), Space: 15.7 MB (52.47%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,36 +2,25 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n= nums.size();
-        int idx=-1;
+        int ind=-1;
         // first find the pivot index
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                 idx=i;
-            break;
-            }
-
-        }
-        if(idx==-1) {
-            reverse(nums.begin(),nums.end());
-            return;
-        }
-          // after pivort we sort the element
-          
-            reverse(nums.begin()+idx+1,nums.end());
-        
-        //after sort we find the just greatest element of pivort element
-        int j=-1;
-        for(int i=idx+1;i<n; i++){
-            if(nums[i]>nums[idx]){
-                j=i;
+                ind=i;
                 break;
             }
         }
-        //swap 
-        int temp=nums[idx];
-        nums[idx]=nums[j];
-        nums[j]=temp;
-     return;
-  
+        if(ind==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        for(int i=n-1;i>ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+        reverse(nums.begin()+ind+1,nums.end());
+      return ;
     }
 };
