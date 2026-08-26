389a74d87088c023b62c371d7182df27f4101a99
sonalipaul <sonalipaul461@gmail.com>
2026-08-26 09:15:48 +0530

    Time: 4 ms (66.51%), Space: 23.8 MB (82.6%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,20 +5,25 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>>ans;
         for(int i=0; i<n;i++){
-            int start=intervals[i][0];
-            int end=intervals[i][1];
-            if(!ans.empty()&& end<=ans.back()[1]){
-                continue;
+            // int start=intervals[i][0];
+            // int end=intervals[i][1];
+            if(ans.empty()|| intervals[i][0]>ans.back()[1]){
+                // continue;
+                ans.push_back (intervals[i]);
             }
-            for(int j=i+1;j<n; j++){
-                if(intervals[j][0]<=end){
-                    end=max(end,intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+            else{
+                ans.back()[1]=max(ans.back()[1], intervals[i][1]);
+
             }
-            ans.push_back({start,end});
+            // for(int j=i+1;j<n; j++){
+            //     if(intervals[j][0]<=end){
+            //         end=max(end,intervals[j][1]);
+            //     }
+            //     else{
+            //         break;
+            //     }
+            // }
+            // ans.push_back({start,end});
 
         }
         return ans;
