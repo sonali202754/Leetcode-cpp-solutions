b408817eff6274147d04e381113147e97e135300
sonalipaul <sonalipaul461@gmail.com>
2026-08-27 12:23:22 +0530

    Time: 0 ms (100%), Space: 25.8 MB (83.96%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int row= grid.size();
-        int col=grid[0].size();
-        int n= row*col;
-       vector<int>hash(n+1,0);
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                hash[grid[i][j]]++;
-            }
+    //     int row= grid.size();
+    //     int col=grid[0].size();
+    //     int n= row*col;
+    //    vector<int>hash(n+1,0);
+    //     for(int i=0;i<row;i++){
+    //         for(int j=0;j<col;j++){
+    //             hash[grid[i][j]]++;
+    //         }
+    //     }
+    //     int rep=-1,miss=-1;
+    //     for(int i=1; i<=n;i++){
+    //         if(hash[i]==2){
+    //             rep=i;
+    //         }
+    //         else if(hash[i]==0){
+    //             miss=i;
+    //         }
+    //         if(rep!=-1&&miss!=-1){
+    //             break;
+    //         }
+    //     }
+    //     return {rep,miss};
+
+    // optimal approach
+    long long row=grid.size();
+    long long col=grid[0].size();
+    long long n=row*col;
+    long long sn=(n*(n+1))/2;
+    long long s2n=(n*(n+1)*(2*n+1))/6;
+    long long s=0,s2=0;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col;j++){
+            s+=grid[i][j];
+            s2+=(long long)grid[i][j]*(long long)grid[i][j];
+
         }
-        int rep=-1,miss=-1;
-        for(int i=1; i<=n;i++){
-            if(hash[i]==2){
-                rep=i;
-            }
-            else if(hash[i]==0){
-                miss=i;
-            }
-            if(rep!=-1&&miss!=-1){
-                break;
-            }
-        }
-        return {rep,miss};
+    }
+        long long val1=s-sn;
+        long long val2=s2-s2n;
+        val2=val2/val1;
+        long long x= (val1+val2)/2;
+        long long y= x-val1;
+        
+    
+    return{(int)x,(int)y};
     }
 };
